b32a740296f6d3d00e52fb4b02c5301e53343d7b
kamus1 <benjamincamus9@gmail.com>
2024-11-13 16:26:16 -0300

    formulario visita

--- src/pages/inscribir_visitas.jsx
+++ src/pages/inscribir_visitas.jsx
@@ -3,11 +3,22 @@ import { useNavigate } from 'react-router-dom';
 
 export const InscribirVisita = () => {
   const [inputValue, setInputValue] = useState('');
+  const [inputValue2, setInputValue2] = useState('');
+  const [inputRut, setInputRut] = useState('');
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
+
+  const handleInputChange2 = (event) => {
+    setInputValue2(event.target.value);
+  };
+
+  const handleInputRutChange = (event) => {
+    setInputRut(event.target.value);
+  };
+
 
   const handleRegisterVisit = () => {
     navigate('/inscripcion-exitosa');
@@ -18,11 +29,28 @@ export const InscribirVisita = () => {
       <h2>Ingrese los datos de su visita</h2>
       <input
         type="text"
-        placeholder="Escribe aquí..."
+        placeholder="Nombre"
         value={inputValue}
         onChange={handleInputChange}
         className="input-visit"
       />
+        <input
+        type="text"
+        placeholder="Apellido"
+        value={inputValue2}
+        onChange={handleInputChange2}
+        className="input-visit"
+      />
+
+      <input
+        type="text"
+        placeholder="Rut"
+        value={inputRut}
+        onChange={handleInputRutChange}
+        className="input-visit"
+      />
+
+
       <button onClick={handleRegisterVisit} className="register-button">
         Registrar visita
       </button>
